b66e469b852c82be65abd6efd7f37be702c08474
Anjuli Goring <anjuligoring@gmail.com>
2021-08-24 11:00:38 -0700

    updating security type for disk resource

--- pkg/engine/template_generator.go
+++ pkg/engine/template_generator.go
@@ -158,7 +158,7 @@ func (t *TemplateGenerator) getTemplateFuncMap(vm *api.APIModel) template.FuncMa
 			case api.TVM:
 				return "SecureBoot"
 			case api.CVM:
-				return "MemoryEncryption"
+				return "ConfidentialVM_DiskEncryptedWithPlatformKey"
 			default:
 				return "None"
 			}
